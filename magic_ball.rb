f7bb0e034be6d54eb92204946bdcae28f6193a0b
CJPaul3 <alex.zephaniah244@gmail.com>
2022-03-17 23:01:12 +0300

    Initial commit

---
+++ magic_ball.rb
@@ -0,0 +1,34 @@
+greetings = [
+  "Hello, dear friend. Answer your question",
+  "If you ask, you’ll get the answer:",
+  "Hello, mortal. The answer for you today is this:"
+]
+answers = [
+  #Положительные:
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes — definitely",
+  "You may rely on it",
+  #Нерешительно положительные
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Signs point to yes",
+  "Yes",
+  #Нейтральные
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  #Отрицательные
+  "Don’t count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+]
+puts greetings.sample
+sleep 3
+puts answers.sample
